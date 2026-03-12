ebba4f5a34eb3921579141983d61739e000ad234
Phoenix <phoenix@playgrounds.dev>
2026-03-13 00:23:17 +0100

    Register post-message

--- apps/chat/src/main.tsx
+++ apps/chat/src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import * as ReactDOM from 'react-dom/client';
 import { initTheme } from './app/theme';
+import './app/postmessage-auth'; // Register postMessage listener before React mounts
 import App from './app/app';
 
 initTheme();
